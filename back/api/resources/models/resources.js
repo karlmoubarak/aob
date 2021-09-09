a763572bf8b948fb940209dfdd9d5ddbbde37e2a
كارل مبارك <bonjour@mouabrak.eu>
2021-09-09 11:19:35 +0200

    slugify native

--- back/api/resources/models/resources.js
+++ back/api/resources/models/resources.js
@@ -1,12 +1,5 @@
 'use strict';
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
- * to customize this model
- */
-
-const slugify = require('slugify');
-
 module.exports = {
   lifecycles: {
     beforeCreate: async (data) => {
