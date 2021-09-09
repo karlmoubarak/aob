b1bbc18e82d1cd2ec20651c782fa82cd2c41cb70
كارل مبارك <bonjour@mouabrak.eu>
2021-09-09 11:17:51 +0200

    slugify native

--- back/api/resources/models/resources.js
+++ back/api/resources/models/resources.js
@@ -11,17 +11,29 @@ module.exports = {
   lifecycles: {
     beforeCreate: async (data) => {
       if (data.Organisation) {
-        data.slug = slugify(data.Organisation);
+        data.slug = await strapi
+        .plugins['content-manager']
+        .services
+        .uid
+        .generateUIDField({
+          contentTypeUID: 'application::resources.resources',
+          field: 'slug',
+          data,
+        })
       }
     },
     beforeUpdate: async (params, data) => {
       if (data.Organisation) {
-        data.slug = slugify(data.Organisation);
+        data.slug = await strapi
+        .plugins['content-manager']
+        .services
+        .uid
+        .generateUIDField({
+          contentTypeUID: 'application::resources.resources',
+          field: 'slug',
+          data,
+        })
       }
     },
   },
 };
-
-
-
-
