afef84df7801a2bb300fb275825174fe09adf0f2
كارل مبارك <bonjour@mouabrak.eu>
2021-09-09 11:21:56 +0200

    slugify native

--- back/api/collections/models/collections.js
+++ back/api/collections/models/collections.js
@@ -1,8 +1,32 @@
 'use strict';
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
- * to customize this model
- */
-
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    beforeCreate: async (data) => {
+      if (data.Title) {
+        data.slug = await strapi
+        .plugins['content-manager']
+        .services
+        .uid
+        .generateUIDField({
+          contentTypeUID: 'application::collections.collections',
+          field: 'slug',
+          data,
+        })
+      }
+    },
+    beforeUpdate: async (params, data) => {
+      if (data.Title) {
+        data.slug = await strapi
+        .plugins['content-manager']
+        .services
+        .uid
+        .generateUIDField({
+          contentTypeUID: 'application::collections.collections',
+          field: 'slug',
+          data,
+        })
+      }
+    },
+  },
+};
