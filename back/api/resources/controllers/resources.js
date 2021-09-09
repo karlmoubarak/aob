a4c09a7f1317a3a45cecf8c5ec7996bdce7000a1
كارل مبارك <bonjour@mouabrak.eu>
2021-09-09 12:24:47 +0200

    tag ad location autocreatio withr esoruces servcies in strapi

--- back/api/resources/controllers/resources.js
+++ back/api/resources/controllers/resources.js
@@ -1,10 +1,67 @@
 'use strict';
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-c  ontrollers)
- * to customize this controller
- */
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 
 module.exports = {
 
+  async create(ctx) {
+    let
+      resource = JSON.parse(ctx.request.body.data),
+      entity,
+      tags = [],
+      locations = []
+
+
+    if (resource.tags.length > 0) {
+      for (let i = 0; i < resource.tags.length; i++) {
+        const tag = resource.tags[i]
+        if (typeof(tag) === 'number') {
+          tags.push(tag)
+        } else {
+          let check = await strapi.query('tags').find({ Name: tag })
+
+          if (check.length > 0 && check[0].Name === tag) {
+            tags.push(check[0].id)
+          } else {
+            let newTag = await strapi.query('tags').create({
+              Name: tag,
+              published_at: null
+            })
+            tags.push(newTag.id)
+          }
+        }
+      }
+      resource.tags = tags
+    }
+
+    if (resource.location.length > 0) {
+      for (let i = 0; i < resource.location.length; i++) {
+        const location = resource.location[i]
+        if (typeof(location) === 'number') {
+          locations.push(location)
+        } else {
+          let check = await strapi.query('locations').find({ Name: location })
+
+          if (check.length > 0 && check[0].Name === location) {
+            location.push(check[0].id)
+          } else {
+            let newLocation = await strapi.query('locations').create({
+              Name: location,
+              published_at: null
+            })
+            locations.push(newLocation.id)
+          }
+        }
+      }
+      resource.locations = locations
+    }
+
+    if (ctx.is('multipart')) {
+      const { files } = parseMultipartData(ctx)
+      entity = await strapi.services.resources.create(resource, { files })
+    } else {
+      entity = await strapi.services.resources.create(resource)
+    }
+    return sanitizeEntity(entity, { model: strapi.models.resources })
+  },
 };
